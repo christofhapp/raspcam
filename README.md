bb2777176deb48796c286eb037835d2911491e0b
christofhapp <51400845+christofhapp@users.noreply.github.com>
2023-02-23 13:32:55 +0100

    asdf

--- README.md
+++ README.md
@@ -1,3 +1,7 @@
 # raspcam
 
 Just take images with a raspberry and a cam
+
+Raspberry Pi: Ubuntu 16 armhf 
+
+diese vorgabe wird für die spinnaker sdk gebraucht
---
+++ blackfly_fotos.py
@@ -0,0 +1,68 @@
+import PySpin
+import cv2
+from datetime import datetime
+import os
+
+def init():
+    if 'BFsys' not in globals():
+        try:
+            global BFsys, BFcamlist
+            BFsys = PySpin.System.GetInstance()
+            BFcamlist = BFsys.GetCameras()
+            print('Blackfly System initialized')
+        except PySpin.SpinnakerException as ex:
+            print('Problem with pyspin.system or system.getcameras: %s' % ex)
+
+        serialRGB = '19346543'
+
+        try:
+            cam = BFcamlist.GetBySerial(serialRGB)
+            if cam.DeviceSerialNumber() == serialRGB:
+                print('Cam RGB initialized')
+        except PySpin.SpinnakerException as ex:
+            print('Cam NOT available: %s' % ex)
+
+        cam.AcquisitionMode.SetValue(PySpin.AcquisitionMode_Continuous)
+
+        if PySpin.IsWritable(cam.SensorShutterMode):
+            cam.SensorShutterMode.SetValue(PySpin.SensorShutterMode_Rolling)  # Rolling, GlobalReset GlobalReset: schafft AutoExposure nicht
+
+    return cam
+
+def getPic(cam):
+    cam.TriggerSoftware.Execute()
+    img_result = cam.GetNextImage()
+    img = img_result.Convert(PySpin.PixelFormat_BGR8)
+    return img.GetNDArray()
+
+def clear(cam):
+    cam.EndAcquisition()
+    cam.TriggerMode.SetValue(PySpin.TriggerMode_Off)
+    cam.DeInit()
+
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+
+    cam = init()
+
+    #cv2.namedWindow('camLWIR', cv2.WINDOW_KEEPRATIO)
+
+
+    path = '/home/pi/Desktop/Fotos/'
+
+    for i in range(10):
+        img = getPic(cam)
+        zeit = datetime.now().strftime("%Y-%m-%d--%H-%M-%S")
+        cv2.imwrite(path + zeit + '.png', img)
+        print(zeit+'.png saved')
+
+    clear(cam)
+
+    #while True:
+    #    img = getPic(cam)
+    #    cv2.imshow('image', img)
+    #    wk = cv2.waitKey(1)
+    #    if wk == ord('q'):
+    #        break
+
+    #cv2.destroyAllWindows()
